464761e0be962cf76c1f04f4bc4e9a96c27be95c
ZenithAtkinson <alexanderdavidatkinson@gmail.com>
2024-09-05 21:20:55 -0600

    Bio files chapter 1

--- Bio364/exercise_break1.3.py
+++ Bio364/exercise_break1.3.py
@@ -4,7 +4,7 @@ import sys
 #Return a space-separated list of starting positions (in increasing order) where 
 #CTTGATCAT appears as a substring in the Vibrio cholerae genome.
 
-def pattern_matching(pattern: str, genome: str) -> list[int]:
+def pattern_matching(pattern: str, genome: str) -> list[int]: #This is only matching a given pattern. Needs to be the complemental reverse (so throw those funcitons in here)
     """Find all occurrences of a pattern in a genome."""
     indicies = []
     temp_index = ""
@@ -14,17 +14,20 @@ def pattern_matching(pattern: str, genome: str) -> list[int]:
             
         if sub_pattern == pattern:
             indicies.append(i)
-
     return indicies
 
-def read_in_file(file_path: str):
+def read_in_file():
     #
-    pass
+    f = open("Vibrio_cholerae.txt", "r")
+    total_genome=f.read()
+    return total_genome
 
 ##### MAIN CALL #####
 
 #The pattern needs to read in the txt file for Vibrio_cholerae. Needs a separate funciton
-pattern = "ATAT"
-genome = "GATATATGCATATACTT"
+genome=read_in_file()
+pattern="CTTGATCAT"
 
-print(pattern_matching(pattern, genome))
+patterns = pattern_matching(pattern, genome)
+for i in patterns:
+    print(i, " ", end = "")
