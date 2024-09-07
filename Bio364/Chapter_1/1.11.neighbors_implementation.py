5f22bc7335437be3f6ca6686ee64ffbe39dca5d7
ZenithAtkinson <alexanderdavidatkinson@gmail.com>
2024-09-07 14:39:53 -0600

    1.11 init

--- Bio364/Chapter_1/1.11.neighbors_implementation.py
+++ Bio364/Chapter_1/1.11.neighbors_implementation.py
@@ -10,8 +10,18 @@ def neighbors(s: str, d: int) -> list[str]:
     if len(s) == 1:
         return {'A', 'C', 'G', 'T'}
     neighborhood = []
-    suffixNeighbors = neighbors(Suffix(s)m d)
+    suffixNeighbors = neighbors(Suffix(s), d)
         #What is suffix? What does it mean?
-
-
+        #I think that Suffix(pattern) is the pattern without its first value (so ACT would just be CT)
+    
+    
     pass
+
+def Suffix(s: str):
+    s_len = len(s)
+    new_s = s[1:s_len]
+    return new_s
+
+pattern = "CGATGGCCTA"
+
+print(Suffix(pattern))
