dda281aca37b8e4c056b0ea2c425536ce4d09dcc
ZenithAtkinson <alexanderdavidatkinson@gmail.com>
2024-09-09 18:49:26 -0600

    CS project1 init

---
+++ CS312/project1/fermat.py
@@ -0,0 +1,60 @@
+import argparse
+import random
+
+
+# This is a convenience function for main(). You don't need to touch it.
+def prime_test(N: int, k: int) -> tuple[str, str]:
+    return fermat(N, k), miller_rabin(N, k)
+
+
+# You will need to implement this function and change the return value.
+def mod_exp(x: int, y: int, N: int) -> int:
+    return 0
+
+
+# You will need to implement this function and change the return value.
+def fprobability(k: int) -> float:
+    return 0
+
+
+# You will need to implement this function and change the return value.
+def mprobability(k: int) -> float:
+    return 0
+
+
+# You will need to implement this function and change the return value, which should be
+# either 'prime' or 'composite'.
+#
+# To generate random values for a, you will most likely want to use
+# random.randint(low, hi) which gives a random integer between low and
+# hi, inclusive.
+def fermat(N: int, k: int) -> str:
+    return "???"
+
+
+# You will need to implement this function and change the return value, which should be
+# either 'prime' or 'composite'.
+#
+# To generate random values for a, you will most likely want to use
+# random.randint(low, hi) which gives a random integer between low and
+# hi, inclusive.
+def miller_rabin(N: int, k: int) -> str:
+    return "???"
+
+
+def main(number: int, k: int):
+    fermat_call, miller_rabin_call = prime_test(number, k)
+    fermat_prob = fprobability(k)
+    mr_prob = mprobability(k)
+
+    print(f'Is {number} prime?')
+    print(f'Fermat: {fermat_call} (prob={fermat_prob})')
+    print(f'Miller-Rabin: {miller_rabin_call} (prob={mr_prob})')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number', type=int)
+    parser.add_argument('k', type=int)
+    args = parser.parse_args()
+    main(args.number, args.k)
---
+++ CS312/project1/rsa.py
@@ -0,0 +1,51 @@
+import random
+import sys
+
+# This may come in handy...
+from fermat import miller_rabin
+
+# If you use a recursive implementation of `mod_exp` or extended-euclid,
+# you recurse once for every bit in the number.
+# If your number is more than 1000 bits, you'll exceed python's recursion limit.
+# Here we raise the limit so the tests can run without any issue.
+# Can you implement `mod_exp` and extended-euclid without recursion?
+sys.setrecursionlimit(4000)
+
+# When trying to find a relatively prime e for (p-1) * (q-1)
+# use this list of 25 primes
+# If none of these work, throw an exception (and let the instructors know!)
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+
+# Implement this function
+def ext_euclid(a: int, b: int) -> tuple[int, int, int]:
+    """
+    The Extended Euclid algorithm
+    Returns x, y , d such that:
+    - d = GCD(a, b)
+    - ax + by = d
+
+    Note: a must be greater than b
+    """
+    return 0, 0, 0
+
+
+# Implement this function
+def generate_large_prime(bits=512) -> int:
+    """
+    Generate a random prime number with the specified bit length.
+    Use random.getrandbits(bits) to generate a random number of the
+     specified bit length.
+    """
+    return 5  # Guaranteed random prime number obtained through fair dice roll
+
+
+# Implement this function
+def generate_key_pairs(bits: int) -> tuple[int, int, int]:
+    """
+    Generate RSA public and private key pairs.
+    Return N, e, d
+    - N must be the product of two random prime numbers p and q
+    - e and d must be multiplicative inverses mod (p-1)(q-1)
+    """
+    return 0, 0, 0
