770743a8ae1597578c0e4ae9a86dd5baf6e77a4b
Alex Atkinson <alexanderdavidatkinson@gmail.com>
2024-09-14 18:21:37 -0600

    Gibbs init

---
+++ Bio364/Chapter_2/2.9.Gibbs_Sampler.py
@@ -0,0 +1,8 @@
+import sys 
+
+# Please do not remove package declarations because these are used by the autograder.
+
+# Insert your gibbs_sampler function here, along with any subroutines you need
+def gibbs_sampler(dna: list[str], k: int, t: int, n: int) -> list[str]:
+    """Implements the GibbsSampling algorithm for motif finding."""
+    pass
