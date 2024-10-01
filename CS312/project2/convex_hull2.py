2785eb44012ccdb58bdf60360ebb1b48c6853e7c
Alex Atkinson <alexanderdavidatkinson@gmail.com>
2024-09-30 19:07:43 -0600

    CS Project 2 progress

--- CS312/project2/convex_hull2.py
+++ CS312/project2/convex_hull2.py
@@ -38,18 +38,8 @@ def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]
     rightHullSize = len(rightHull)
 
     # find the greatest/least slopes (+/-) between the keyPoint on the left and any point within the right hull #
-    for i in range(rightHullSize):
-        # looking for the extreme slopes #
-        greatestPositiveSlope = calculateSlope(keyPoint_left, upperTangent_right)
-        greatestNegativeSlope = calculateSlope(keyPoint_left, lowerTangent_right)
+ 
 
-        if calculateSlope(keyPoint_left, rightHull[i]) > greatestPositiveSlope:
-            upperTangent_right = rightHull[i]
-
-        if calculateSlope(keyPoint_left, rightHull[i]) < greatestNegativeSlope:
-            lowerTangent_right = rightHull[i]
-
-            
 
     # find the smallest slope (-) between all points on the left and the newly assigned keyPoint on the right hull #
     for i in range(leftHullSize):
@@ -81,3 +71,47 @@ def calculateSlope(left: tuple[float, float], right: tuple[float, float]):
     rise = right[1] - left[1] # right's Y chord - left's Y chord
     run = right[0] - left[0] # right's X chord - left's X chord
     return rise/run
+
+
+#L is equal to our left hull
+#R is equal to our right hull
+def find_upper_tangent(L, R):
+    # Find the rightmost point in L and the leftmost point in R
+    pperTangent_left = L[-1]   # Rightmost point of left hull
+    upperTangent_right = R[0]  
+    
+    def is_upper_tangent(p, q, L):
+        # Check if line pq is the upper tangent to L
+        idx_p = L.index(p)
+        prev_p = L[idx_p - 1] if idx_p > 0 else L[-1]
+        next_p = L[(idx_p + 1) % len(L)]
+        return (cross_product(p, q, prev_p) >= 0 and cross_product(p, q, next_p) >= 0)
+
+    def cross_product(o, a, b):
+        # Cross product of vectors OA and OB
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    done = False
+    while not done:
+        done = True
+        
+        # Move p counterclockwise in L while pq is not an upper tangent to L
+        while not is_upper_tangent(p, q, L):
+            p_idx = L.index(p)
+            p = L[(p_idx - 1) % len(L)]  # Move counterclockwise in L
+            done = False
+        
+        # Move q clockwise in R while pq is not an upper tangent to R
+        while not is_upper_tangent(q, p, R):  # Here we reverse the order to check tangent on R
+            q_idx = R.index(q)
+            q = R[(q_idx + 1) % len(R)]  # Move clockwise in R
+            done = False
+
+    return (p, q)
+
+# Example input convex hulls (list of points) L and R
+L = [(1, 1), (2, 3), (3, 4), (4, 2)]
+R = [(5, 2), (6, 3), (7, 1)]
+
+upper_tangent = find_upper_tangent(L, R)
+print("Upper Tangent Points:", upper_tangent)
