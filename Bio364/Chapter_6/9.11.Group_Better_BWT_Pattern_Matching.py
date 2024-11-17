86ca71ca02be6c1a27271d4f1c9fb0717dfb8d3c
Alex Atkinson <alexanderdavidatkinson@gmail.com>
2024-11-17 14:44:00 -0700

    9.11 init2

--- Bio364/Chapter_6/9.11.Group_Better_BWT_Pattern_Matching.py
+++ Bio364/Chapter_6/9.11.Group_Better_BWT_Pattern_Matching.py
@@ -8,4 +8,29 @@ def better_bw_matching(bwt: str, patterns: List[str]) -> List[int]:
     """
     Perform an optimized Burrows-Wheeler Matching for a set of patterns against the Burrows-Wheeler Transform of a text.
     """
+    
     pass
+
+# Sample input:
+# GGCGCCGC$TAGTCACACACGCCGTA
+# ACC CCG CAG
+
+# Sample output:
+# 1 2 1
+
+'''
+BetterBWMatching(FirstOccurrence, LastColumn, Pattern, Count)
+    top ← 0
+    bottom ← |LastColumn| − 1
+    while top ≤ bottom
+        if Pattern is nonempty
+            symbol ← last letter in Pattern
+            remove last letter from Pattern
+            if positions from top to bottom in LastColumn contain an occurrence of symbol
+                top ← FirstOccurrence(symbol) + Countsymbol(top, LastColumn)
+                bottom ← FirstOccurrence(symbol) + Countsymbol(bottom + 1, LastColumn) − 1
+            else
+                return 0
+        else
+            return bottom − top + 1
+'''
