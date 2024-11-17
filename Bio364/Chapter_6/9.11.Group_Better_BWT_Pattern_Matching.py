503bdd1bfdd775ee025da16688ff28d23dc27124
Alex Atkinson <alexanderdavidatkinson@gmail.com>
2024-11-17 13:53:26 -0700

    9.11 bio init

---
+++ Bio364/Chapter_6/9.11.Group_Better_BWT_Pattern_Matching.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List, Dict, Iterable, Tuple
+
+# Please do not remove package declarations because these are used by the autograder.
+
+# Insert your better_bw_matching function here, along with any subroutines you need
+def better_bw_matching(bwt: str, patterns: List[str]) -> List[int]:
+    """
+    Perform an optimized Burrows-Wheeler Matching for a set of patterns against the Burrows-Wheeler Transform of a text.
+    """
+    pass
